38e4f68c9a4ee559025fa7cef50084f25600756a
Shaily Agarwal <shailyagarwal340@gmail.com>
2021-02-18 17:59:20 +0530

    changed-accountjs

--- frontend/pages/account.js
+++ frontend/pages/account.js
@@ -1,6 +1,18 @@
+import {useUser} from '../components/User';
+
 export default function OrderPage() {
-  return <div>
+  const me = useUser();
+  if (!me){
+    return (<div>
     <p>Hello!</p>
-  </div>
+    </div>)
+  }
+  return(
+    <div>
+      <h1>Hello {me.name}</h1>
+      <h3>Your registered email: {me.email}</h3>
+    </div>
+  );
+  
 }
 
